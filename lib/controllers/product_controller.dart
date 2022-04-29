d1805c81f2bb24ab58b6ad16424eeb11a1051224
Sakib <kazisakib556@gmail.com>
2022-04-29 16:12:51 +0600

    20% completed

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -2,5 +2,6 @@ import 'package:flutter_ecommerce/models/GetSampleApi.dart';
 import 'package:get/state_manager.dart';
 class ProductController extends GetxController{
   var productList = <Stackdata>[].obs;
+  var a;
 
 }
