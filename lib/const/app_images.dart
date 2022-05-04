2c5ca997e4dddeb5bc511c4b2ed9ece516481f5a
Sakib <kazisakib556@gmail.com>
2022-05-04 23:39:24 +0600

    23% completed

---
+++ lib/const/app_images.dart
@@ -0,0 +1,6 @@
+
+class AppImages{
+  static const String LOGIN_BACKGROUND= "assets/images/login.png";
+  static const String REGISTER_BACKGROUND= "assets/images/register.png";
+  static const String PRODUCT_PANJABI= "assets/images/product";
+}
