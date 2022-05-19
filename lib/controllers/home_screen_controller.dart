e89a124e45dfd6eeb94f651b71804fbd3ea12dd2
Sakib <kazisakib556@gmail.com>
2022-05-19 22:05:42 +0600

    home_screen set up

--- lib/controllers/home_screen_controller.dart
+++ lib/controllers/home_screen_controller.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_ecommerce/controllers/signup_controllert.dart';
+import 'package:flutter_ecommerce/controllers/signup_controller.dart';
 import 'package:get/get.dart';
 
 class HomeScreenController extends GetxController {
